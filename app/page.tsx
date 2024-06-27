9ec031c0a8d7ce6ab14c4a1a8a308263ea21239f
Pelumi Matti <pelumimatti@Pelumis-MacBook-Pro.local>
2024-06-27 21:57:11 +0100

    progress on feat section 80%

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
+import Feats from '@/components/Feats';
 import Hero from '@/components/Hero';
 import Navbar from '@/components/Navbar';
-import Image from 'next/image';
 
 export default function Home() {
 	return (
 		<div>
 			<Navbar />
 			<Hero />
+			<Feats />
 		</div>
 	);
 }
